a0b8be42d82c88b49c4b29cead1bcc58ef0721bf
Mikkel Lindblom <mikkel.lindblom@gmail.com>
2026-03-15 11:44:31 +0100

    initial commit

---
+++ config/scaleway-tem.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'project_id' => env('SCALEWAY_TEM_PROJECT_ID'),
+    'api_key' => env('SCALEWAY_TEM_API_KEY'),
+    'region' => env('SCALEWAY_TEM_REGION'),
+];
---
+++ src/LaravelScalewayTemServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Moxso\LaravelScalewayTem;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Mail;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Symfony\Component\Mailer\Bridge\Scaleway\Transport\ScalewayTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
+
+class LaravelScalewayTemServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('laravel-scaleway-tem')
+            ->hasConfigFile();
+    }
+
+    public function bootingPackage(): void
+    {
+        Mail::extend('scaleway', function (array $config) {
+            return (new ScalewayTransportFactory)->create(
+                new Dsn('scaleway+api', 'default',
+                    data_get($config, 'project_id') ?? Config::get('scaleway-tem.project_id'),
+                    data_get($config, 'api_key') ?? Config::get('scaleway-tem.api_key'),
+                    options: [
+                        'region' => data_get($config, 'options.region') ?? Config::get('scaleway-tem.region'),
+                    ]
+                )
+            );
+        });
+    }
+}
